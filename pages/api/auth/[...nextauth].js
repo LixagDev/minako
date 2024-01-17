f5f7e58c828069ffb011b9b406274794c2eb2d2d
Lixag <valent1.arn@gmail.com>
2024-01-17 18:55:06 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,6 +17,27 @@ export const authOptions = {
     pages:{
         signIn: "/"
     },
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            const isAllowedToSignIn = true;
+            if (isAllowedToSignIn) {
+                const updateDiscordAvatar = await prisma.user.update({
+                    where:{
+                        id: user.id
+                    },
+                    data:{
+                        image: profile.image_url
+                    }
+                });
+                return true
+            } else {
+                // Return false to display a default error message
+                return false
+                // Or you can return a URL to redirect to:
+                // return '/unauthorized'
+            }
+        }
+    }
 }
 
 export default NextAuth(authOptions)
