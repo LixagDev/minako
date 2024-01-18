ce40da0679de122ba09f9ceb5d7e287f0c0d8d0d
Lixag <valent1.arn@gmail.com>
2024-01-18 11:18:30 +0100

    Add Router Refresh

--- functions/DeleteMessage.js
+++ functions/DeleteMessage.js
@@ -2,5 +2,6 @@ import {useRouter} from "next/navigation";
 import axios from "axios";
 
 export default function DeleteMessage(messageId, userSessionData){
-    return axios.post("/api/delete/message", {messageId: messageId, ownerId: userSessionData.id})
+    const router = useRouter();
+    return axios.post("/api/delete/message", {messageId: messageId, ownerId: userSessionData.id}).then(() => router.refresh());
 }
